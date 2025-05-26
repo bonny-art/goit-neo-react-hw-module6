aa302fdfb5fef864a1c3a2e0bb8dce4034f16706
bonny-art <svitlana.otenko@gmail.com>
2025-05-26 17:58:40 +0300

    Add imports

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { Toaster } from "react-hot-toast";
 import "./index.css";
 import App from "./App.jsx";
 import { persistor, store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
